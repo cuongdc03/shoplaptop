61beb760cf01e7d9efe20e6500dd4f42c19a2ee9
Cao-Cuong Do <88456061+cuongdc03@users.noreply.github.com>
2023-05-06 12:46:36 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Demo.Data;
+using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DemoContext _context;
+
+        public HomeController(DemoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Product.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
